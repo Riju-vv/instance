7821df28151b469af90d2caa43fe7f277e000b5c
Riju-vv <riju.vv@progress.com>
2021-11-23 06:47:33 +0000

    End Of Lab: Launching VMs

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ key = "student.09-network-state"
 region = "eu-west-2"
 }
 }
+
+resource "aws_instance" "my_vm" {
+ami = "ami-0fdf70ed5c34c5f52"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+instance_type = "t3.micro"
+tags = {
+Name = "student.09-vm1"
+}
+}
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "my_vmip" {
+value = aws_instance.my_vm.public_ip
+}
+
