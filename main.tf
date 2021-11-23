74092d505a5e868f67b26fdd7e0e4b3f90a1f4c9
Riju-vv <riju.vv@progress.com>
2021-11-23 10:38:28 +0000

    End Of Lab: Launching Multiple VMs

--- main.tf
+++ main.tf
@@ -16,6 +16,20 @@ key_name = data.terraform_remote_state.network_details.outputs.my_keyname
 subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 tags = {
-Name = "student.09-vm1"
+Name = "student.09-webserver-vm"
 }
 }
+
+module "loadbalancer" {
+source = "./modules/linux_node"
+ami = "ami-0fdf70ed5c34c5f52"
+instance_type = "t3.micro"
+instance_count = "0"
+key_name = data.terraform_remote_state.network_details.outputs.my_keyname
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
+tags = {
+Name = "student.09-loadbalancer-vm"
+}
+}
+
