9e77674c30ff0cf6e4b1016306df73a8241ae59e
Riju-vv <riju.vv@progress.com>
2021-11-23 07:55:52 +0000

    attaching security group

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ ami = "ami-0fdf70ed5c34c5f52"
 subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 instance_type = "t3.micro"
 key_name = data.terraform_remote_state.network_details.outputs.my_keyname
+vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 tags = {
 Name = "student.09-vm1"
 }
