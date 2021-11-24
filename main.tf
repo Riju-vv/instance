fad46f728b6d7efc6bf519a6af336ac8e93c4d1f
Riju-vv <riju.vv@progress.com>
2021-11-24 12:37:04 +0000

    changed web docker host to 2

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "web_docker_host" {
 source = "./modules/linux_node"
 ami = "ami-0fdf70ed5c34c5f52"
 instance_type = "t3.micro"
-instance_count = "1"
+instance_count = "2"
 key_name = data.terraform_remote_state.network_details.outputs.my_keyname
 subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
